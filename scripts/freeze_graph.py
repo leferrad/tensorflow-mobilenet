0e495b4baaf697161e0e681920bbc688937fbf5d
Leandro Ferrado <ljferrado@gmail.com>
2017-10-03 01:22:55 -0300

    Updating scripts with the new refactor

--- scripts/freeze_graph.py
+++ scripts/freeze_graph.py
@@ -1,26 +1,17 @@
 from __future__ import print_function
 
-from utils.fileio import download_and_uncompress_tarball, get_logger, is_valid_slim_directory, save_dict_as_json, \
-                         MobileNetDefaultFile
-
-from tensorflow.python.framework import graph_util
-import tensorflow as tf
+from mobilenet.core import MobileNetDefaultFile, MobileNetV1Restored
+from mobilenet.fileio import download_and_uncompress_tarball, get_logger
+from mobilenet.util import create_readable_names_for_imagenet_labels
 
 import os
 import re
 import json
-from sys import path as syspath
 import argparse
-
-slim = tf.contrib.slim
 
 
 def create_label_json_file(json_fn):
-    # From tensorflow/models/slim
-    from datasets import imagenet
-    # TODO: don't depend on slim for this
-
-    labels = imagenet.create_readable_names_for_imagenet_labels()
+    labels = create_readable_names_for_imagenet_labels()
 
     with open(json_fn, 'w') as f:
         json.dump(labels, f,
@@ -30,78 +21,15 @@ def create_label_json_file(json_fn):
 
     return labels
 
-def freeze_mobilenet(meta_file, img_size=224, model_factor=1.0, weight_decay=0.0, num_classes=1001):
-    # tf.reset_default_graph()
-
-    # Insert Input placeholder for images
-    inp = tf.placeholder(tf.float32, shape=(None, img_size, img_size, 3), name="input")
-    arg_scope = mobilenet_v1.mobilenet_v1_arg_scope(weight_decay=weight_decay)
-    with slim.arg_scope(arg_scope):
-        logits, end_points = mobilenet_v1.mobilenet_v1(inp, num_classes=num_classes,
-                                                       is_training=False,
-                                                       depth_multiplier=model_factor)
-
-    predictions = tf.contrib.layers.softmax(logits)
-    output = tf.identity(predictions, name='output')
-
-    # We retrieve the protobuf graph definition
-    graph = tf.get_default_graph()
-    input_graph_def = graph.as_graph_def()
-
-    # We retrieve our checkpoint fullpath
-    input_checkpoint = meta_file.replace('.meta', '')
-    output_graph_fn = input_checkpoint.replace('.ckpt', '.pb')
-    output_node_names = "output"
-
-    # Add ops to restore all the variables
-    rest_var = slim.get_variables_to_restore()  # TODO: change this! maybe it can optimize the final network
-
-    # We start a session and restore the graph weights
-    with tf.Session() as sess:
-
-        saver = tf.train.Saver(rest_var)
-        saver.restore(sess, input_checkpoint)
-
-        # We use a built-in TF helper to export variables to constants
-        output_graph_def = graph_util.convert_variables_to_constants(
-            sess,  # The session is used to retrieve the weights
-            input_graph_def,  # The graph_def is used to retrieve the nodes
-            output_node_names.split(",")  # The output node names are used to select the useful nodes
-        )
-
-        # Finally we serialize and dump the output graph to the filesystem
-        with tf.gfile.GFile(output_graph_fn, "wb") as f:
-            f.write(output_graph_def.SerializeToString())
-        logger.info("%i ops in the final graph.", len(output_graph_def.node))
-
 logger = get_logger(name="freeze_mobilenet", level='debug')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-sdir', '--slim-dir', dest='slim_dir', default='/root/tensorflow/models/research/slim',
-                        help="Path to directory with TensorFlow's Slim models.")
     parser.add_argument('-ckpt', '--checkpoint-path', dest='checkpoint_path',
                         default='',
                         help="Path to directory with the checkpoints of the MobileNet model")
     args = parser.parse_args()
-
-    #slim_dir = args.slim_dir
-    slim_dir = '/home/leeandro04/work/tensorflow-stuff/models/research/slim'
-
-    slim_dir_is_ok = slim_dir is not None and os.path.exists(slim_dir) and is_valid_slim_directory(slim_dir)
-
-    if not slim_dir_is_ok:
-        logger.error("Argument '-sdir' not valid! Please enter the correct path to Slim directory. "
-                     "You can download it by: 'git clone https://github.com/tensorflow/models.git'")
-        exit(0)
-
-    # Adding this path to PYTHONPATH for importing some modules
-    syspath.append(slim_dir)
-
-    # Now we can import the modules from Slim
-    # From tensorflow/models/slim
-    from nets import mobilenet_v1
 
     #checkpoint_path = args.checkpoint_path
     checkpoint_path = '/home/leeandro04/Escritorio/NEW_mobilenet_v1_1.0_224_2017_06_14'
@@ -161,18 +89,19 @@ if __name__ == '__main__':
 
     model_pb = MobileNetDefaultFile.MODEL_PB_FMT.format(factor, img_size)
 
-    meta_file = model_pb.replace('.pb', '.ckpt.meta')
+    checkpoint_file = model_pb.replace('.pb', '.ckpt')
 
-    meta_file = os.path.join(checkpoint_path, meta_file)
+    checkpoint_file = os.path.join(checkpoint_path, checkpoint_file)
 
     try:
-        if os.path.exists(meta_file):
-            logger.info("Processing meta_file '%s' ...", meta_file)
-            freeze_mobilenet(meta_file, int(img_size), float(factor), num_classes=1001)
+        if os.path.exists(checkpoint_file+'.meta'):
+            logger.info("Processing checkpoint file '%s' ...", checkpoint_file)
+            mobilenet_model = MobileNetV1Restored(img_size=int(img_size), model_factor=float(factor))
+            mobilenet_model.freeze_inference_graph(checkpoint_file, output_filename='frozen_graph.pb')
             create_label_json_file('/tmp/mobilenet/labels.json')
             #copyfile(os.path.join(model_dir, model_pb), os.path.join(img_subdir, model_pb))
         else:
-            logger.info("Skipping not existing meta file '%s'...", meta_file)
+            logger.info("Skipping not existing meta file '%s'...", checkpoint_file)
             pass
     except Exception as e:
-        logger.error("Failed to process meta_file '%s': %s", meta_file, str(e))
+        logger.error("Failed to process meta_file '%s': %s", checkpoint_file, str(e))
--- scripts/make_inferences.py
+++ scripts/make_inferences.py
@@ -1,135 +1,13 @@
 from __future__ import print_function
 
-from utils.fileio import get_logger, load_json_as_dict
-
-from tensorflow.python.platform import gfile
-import tensorflow as tf
-
-from scipy.misc import imread, imresize
-import numpy as np
+from mobilenet.core import MobileNetV1Restored
+from mobilenet.fileio import get_logger
 
 import os
-import time
 import argparse
-
-slim = tf.contrib.slim
 
 
 logger = get_logger(name="make_inferences", level='debug')
-
-
-def load_labels():
-    path = '../labels.json'
-    labels = load_json_as_dict(path)
-    return labels
-
-
-def read_tensor_from_image_file(file_name, input_height=224, input_width=224, input_mean=-127, input_std=127):
-    input_name = "file_reader"
-    output_name = "normalized"
-    file_reader = tf.read_file(file_name, input_name)
-
-    if file_name.endswith(".png"):
-        image_reader = tf.image.decode_png(file_reader, channels=3, name='png_reader')
-    elif file_name.endswith(".gif"):
-        image_reader = tf.squeeze(tf.image.decode_gif(file_reader, name='gif_reader'))
-    elif file_name.endswith(".bmp"):
-        image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
-    else:
-        image_reader = tf.image.decode_jpeg(file_reader, channels=3, name='jpeg_reader')
-
-    float_caster = tf.cast(image_reader, tf.float32)
-    dims_expander = tf.expand_dims(float_caster, 0)
-    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-    normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-    sess = tf.Session()
-    result = sess.run(normalized)
-
-    return result
-
-def report_output(img_filename, prediction, labels, n_top=10):
-    logger.info("Top %i predictions for the image given by '%s':", n_top, img_filename)
-    top_predictions = sorted(enumerate(prediction), key=lambda (i, p): -p)[:n_top]
-    c = 1
-    for i, p in top_predictions:
-        l = labels[str(i)]
-        logger.info("%i. %s (prob=%.5f)", c, l, p)
-        c += 1
-
-
-def load_and_prepare_image(filename, img_size=224):
-    """
-    NOTE: don't use this!!
-
-    :param filename:
-    :param img_size:
-    :return:
-    """
-    #img = np.asarray(Image.open(filename).convert('RGB'))
-    img = imread(filename, flatten=False)
-    img = imresize(img, (img_size, img_size))
-    img = img.astype(np.float32)
-    img = np.expand_dims(img, 0)
-
-    # Preprocess
-    img = (img - 127.) / 127.
-
-    return img
-
-def load_graph(model_file):
-    graph = tf.Graph()
-    graph_def = tf.GraphDef()
-
-    with open(model_file, "rb") as f:
-        graph_def.ParseFromString(f.read())
-    with graph.as_default():
-        tf.import_graph_def(
-            graph_def,
-            input_map=None,
-            return_elements=None,
-            name="mobilenet",
-            op_dict=None,
-            producer_op_list=None
-        )
-
-    return graph
-
-
-def load_mobilenet(model_filename, images_path, logs_directory='/tmp/tf-logs'):
-
-    # Now load model and make inferences
-    predictions = []
-    labels = load_labels()
-
-    graph = load_graph(model_filename)
-
-    tf.summary.FileWriter(logs_directory, graph=graph)
-
-    with tf.Session(graph=graph) as sess:
-
-        input_tensor = graph.get_tensor_by_name("mobilenet/input:0")
-        softmax_tensor = graph.get_tensor_by_name("mobilenet/MobilenetV1/Predictions/Reshape_1:0")
-        avg_pool_tensor = graph.get_tensor_by_name("mobilenet/MobilenetV1/Logits/AvgPool_1a/AvgPool:0")
-
-        for im in os.listdir(images_path):
-            tic = time.time()
-            img = load_and_prepare_image(os.path.join(images_path, im))
-            #img = read_tensor_from_image_file(os.path.join(images_path, im))
-
-            prediction, embedding = sess.run([softmax_tensor, avg_pool_tensor], {input_tensor: img})
-            prediction = np.squeeze(prediction)
-            embedding = np.squeeze(embedding)
-
-
-            report_output(im, prediction, labels=labels, n_top=10)
-
-            toc = time.time()
-
-            print("Detection made in %.2f sec" % (toc-tic))
-
-            predictions.append((im, embedding))
-
-    return predictions
 
 
 if __name__ == '__main__':
@@ -142,9 +20,9 @@ if __name__ == '__main__':
 
     #pb_path = args.pb_path
 
-    pb_path = '/home/leeandro04/Escritorio/NEW_mobilenet_v1_1.0_224_2017_06_14/mobilenet_v1_final.pb'
-    img_path = '/home/leeandro04/work/poc_lbp_pic/img/whatsapp/'
-    #img_path = '/home/leeandro04/Escritorio/imgs/'
+    pb_path = '/home/leeandro04/Escritorio/NEW_mobilenet_v1_1.0_224_2017_06_14/frozen_graph.pb'
+    #img_path = '/home/leeandro04/work/poc_lbp_pic/img/whatsapp/'
+    img_path = '/home/leeandro04/Escritorio/imgs/'
 
     if not os.path.exists(pb_path):
         logger.error("Argument '-pb' not valid! Please enter the correct path to the MobileNet protobuf model.")
@@ -152,9 +30,10 @@ if __name__ == '__main__':
 
     try:
         logger.info("Processing meta_file '%s' ...", pb_path)
-        embeddings = load_mobilenet(pb_path, img_path)
+        mobilenet_model = MobileNetV1Restored(img_size=224, model_factor=1.0)
+        predictions = mobilenet_model.load_and_predict_on_images(pb_path, img_path)
 
-        print("DONE")
+        logger.info("DONE")
 
     except Exception as e:
         logger.error("Failed to process pb_file '%s': %s", pb_path, str(e))
