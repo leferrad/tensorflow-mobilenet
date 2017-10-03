b7f27ea21e3c2b00ba621335972244453d4d8b9b
Leandro Ferrado <ljferrado@gmail.com>
2017-10-02 22:09:16 -0300

    Updates on script freeze_graph

--- scripts/freeze_graph.py
+++ scripts/freeze_graph.py
@@ -1,61 +1,78 @@
 from __future__ import print_function
 
-from utils.fileio import download_and_uncompress_tarball, get_logger, is_valid_slim_directory, MobileNetDefaultFile
+from utils.fileio import download_and_uncompress_tarball, get_logger, is_valid_slim_directory, save_dict_as_json, \
+                         MobileNetDefaultFile
 
 from tensorflow.python.framework import graph_util
 import tensorflow as tf
 
 import os
 import re
-from shutil import copyfile
+import json
 from sys import path as syspath
 import argparse
 
 slim = tf.contrib.slim
 
 
-def freeze_mobilenet(meta_file, img_size=224, model_factor=1.0, num_classes=1001):
+def create_label_json_file(json_fn):
+    # From tensorflow/models/slim
+    from datasets import imagenet
+    # TODO: don't depend on slim for this
 
-    tf.reset_default_graph()
+    labels = imagenet.create_readable_names_for_imagenet_labels()
 
+    with open(json_fn, 'w') as f:
+        json.dump(labels, f,
+                  sort_keys=True,
+                  indent=4,
+                  separators=(',', ': '))
+
+    return labels
+
+def freeze_mobilenet(meta_file, img_size=224, model_factor=1.0, weight_decay=0.0, num_classes=1001):
+    # tf.reset_default_graph()
+
+    # Insert Input placeholder for images
     inp = tf.placeholder(tf.float32, shape=(None, img_size, img_size, 3), name="input")
-    weight_decay = 0.0
     arg_scope = mobilenet_v1.mobilenet_v1_arg_scope(weight_decay=weight_decay)
     with slim.arg_scope(arg_scope):
-        logits, _ = mobilenet_v1.mobilenet_v1(inp, num_classes=num_classes,
-                                              is_training=False,
-                                              depth_multiplier=model_factor)
+        logits, end_points = mobilenet_v1.mobilenet_v1(inp, num_classes=num_classes,
+                                                       is_training=False,
+                                                       depth_multiplier=model_factor)
 
     predictions = tf.contrib.layers.softmax(logits)
     output = tf.identity(predictions, name='output')
 
-    #tf.summary.FileWriter('/tmp/tf-logs/', graph=output.graph)
+    # We retrieve the protobuf graph definition
+    graph = tf.get_default_graph()
+    input_graph_def = graph.as_graph_def()
 
-    ckpt_file = meta_file.replace('.meta', '')
-    output_graph_fn = ckpt_file.replace('.ckpt', '.pb')
+    # We retrieve our checkpoint fullpath
+    input_checkpoint = meta_file.replace('.meta', '')
+    output_graph_fn = input_checkpoint.replace('.ckpt', '.pb')
     output_node_names = "output"
 
-    rest_var = slim.get_variables_to_restore()
+    # Add ops to restore all the variables
+    rest_var = slim.get_variables_to_restore()  # TODO: change this! maybe it can optimize the final network
 
+    # We start a session and restore the graph weights
     with tf.Session() as sess:
-        graph = tf.get_default_graph()
-        input_graph_def = graph.as_graph_def()
 
         saver = tf.train.Saver(rest_var)
-        saver.restore(sess, ckpt_file)
+        saver.restore(sess, input_checkpoint)
 
-        # We use a built-in TF helper to export variables to constant
+        # We use a built-in TF helper to export variables to constants
         output_graph_def = graph_util.convert_variables_to_constants(
-            sess, # The session is used to retrieve the weights
-            input_graph_def, # The graph_def is used to retrieve the nodes
-            output_node_names.split(",") # The output node names are used to select the usefull nodes
+            sess,  # The session is used to retrieve the weights
+            input_graph_def,  # The graph_def is used to retrieve the nodes
+            output_node_names.split(",")  # The output node names are used to select the useful nodes
         )
 
         # Finally we serialize and dump the output graph to the filesystem
         with tf.gfile.GFile(output_graph_fn, "wb") as f:
             f.write(output_graph_def.SerializeToString())
         logger.info("%i ops in the final graph.", len(output_graph_def.node))
-
 
 logger = get_logger(name="freeze_mobilenet", level='debug')
 
@@ -69,7 +86,8 @@ if __name__ == '__main__':
                         help="Path to directory with the checkpoints of the MobileNet model")
     args = parser.parse_args()
 
-    slim_dir = args.slim_dir
+    #slim_dir = args.slim_dir
+    slim_dir = '/home/leeandro04/work/tensorflow-stuff/models/research/slim'
 
     slim_dir_is_ok = slim_dir is not None and os.path.exists(slim_dir) and is_valid_slim_directory(slim_dir)
 
@@ -85,7 +103,8 @@ if __name__ == '__main__':
     # From tensorflow/models/slim
     from nets import mobilenet_v1
 
-    checkpoint_path = args.checkpoint_path
+    #checkpoint_path = args.checkpoint_path
+    checkpoint_path = '/home/leeandro04/Escritorio/NEW_mobilenet_v1_1.0_224_2017_06_14'
 
     if checkpoint_path == '':
         # Then we have to download a default model
@@ -150,6 +169,7 @@ if __name__ == '__main__':
         if os.path.exists(meta_file):
             logger.info("Processing meta_file '%s' ...", meta_file)
             freeze_mobilenet(meta_file, int(img_size), float(factor), num_classes=1001)
+            create_label_json_file('/tmp/mobilenet/labels.json')
             #copyfile(os.path.join(model_dir, model_pb), os.path.join(img_subdir, model_pb))
         else:
             logger.info("Skipping not existing meta file '%s'...", meta_file)
