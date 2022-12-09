c2a235b2580a6874d641a188185b933451f6bd56
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-09 01:34:17 +0000

    Adding tarfile member sanitization to extractall()

--- mobilenet/fileio.py
+++ mobilenet/fileio.py
@@ -67,7 +67,26 @@ def compress_tar_files(files, filename):
 def uncompress_tar_files(filename):
     try:
         with tarfile.open(filename, "r:*") as tar:
-            tar.extractall(path=os.path.dirname(filename))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=os.path.dirname(filename))
         successful = True
     except:
         successful = False
